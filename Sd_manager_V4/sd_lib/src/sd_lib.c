6c759c217f9faddfcd184836597aba3f4c0fd2ed
Daniel Gutiérrez Torres <82153822+guti10x@users.noreply.github.com>
2024-07-12 15:49:45 +0200

    Add función verificarSd() to sd_lib (Para añadir una doble verificación de que se ha abierto correctamnete y se puede guardar correctamente en el archivo de la sd

--- Sd_manager_V4/sd_lib/src/sd_lib.c
+++ Sd_manager_V4/sd_lib/src/sd_lib.c
@@ -2,6 +2,16 @@
 #include "main.h"
 #include <stdio.h>
 
+/**
+ * @brief Guarda datos en un archivo CSV en la tarjeta SD.
+ *
+ * Esta función abre (o crea si no existe) un archivo llamado "data.csv" en la tarjeta SD,
+ * se posiciona al final del archivo y escribe una nueva línea con los datos proporcionados.
+ *
+ * @param id El identificador que se va a guardar.
+ * @param value El valor que se va a guardar.
+ * @param timestamp La marca de tiempo asociada a los datos.
+ */
 void save_sd(int id, const char* value, const char* timestamp) {
     FIL fil;  // Variable para guardar fivhero
     char buffer[256]; // Variable para guardar fila a insertar
@@ -10,13 +20,14 @@ void save_sd(int id, const char* value, const char* timestamp) {
     if (f_open(&fil, "data.csv", FA_OPEN_ALWAYS | FA_WRITE) != FR_OK) {
         // Manejar el error de apertura o creación del archivo
         Error_Handler();
+
         return;
     }
 
     // Posicionarse al final del archivo para añadir datos
     if (f_lseek(&fil, f_size(&fil)) != FR_OK) {
         // Manejar error
-        f_close(&fil);
+        f_close(&fil) ;
         Error_Handler();
         return;
     }
@@ -33,3 +44,33 @@ void save_sd(int id, const char* value, const char* timestamp) {
     // Cerrar el archivo
     f_close(&fil);
 }
+
+/**
+ * @brief Verifica si un archivo puede ser abierto y escrito.
+ *
+ * Esta función intenta abrir un archivo en modo de escritura y verifica si el archivo se puede escribir.
+ *
+ * @param filename nombre del archivo a verificar.
+ * @return int Devuelve 1 si el archivo puede ser abierto y escrito correctamente, de lo contrario devuelve 0.
+ */
+int verificarSd(const char* filename) {
+    FIL fil;
+    FRESULT res;
+
+    // Intentar abrir el archivo en modo de escritura
+    res = f_open(&fil, filename, FA_OPEN_ALWAYS | FA_WRITE);
+    if (res != FR_OK) {
+        return 0;
+    }
+
+    // Verificar si el archivo está abierto y se puede escribir
+    if ((fil.flag & FA_WRITE) == 0) {
+        f_close(&fil);
+        return 0;
+    }
+
+    // Cerrar el archivo y devolver 1 (todo ok)
+    f_close(&fil);
+    return 1;
+}
+
